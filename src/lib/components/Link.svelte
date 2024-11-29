9497ce17ed04736fe848055db1abc34eee90bdeb
safar190305 <safar190305@gmail.com>
2024-11-29 16:33:27 +0700

    to svelte kit

---
+++ src/lib/components/Link.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	let { to }: { to: string } = $props();
+</script>
+
+<a
+	href={to}
+	target="_blank"
+	rel="noopener noreferrer"
+	class="font-semibold text-blue-500 hover:underline"
+>
+	<slot />
+</a>
